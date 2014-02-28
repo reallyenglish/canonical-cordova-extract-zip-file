6cdf6667763549bf69e030d518c77d92ef0c83ca
joey <joeyw@reallyenglish.com>
2014-02-28 01:27:49 +0000

    code to unzip files for Android added

---
+++ src/android/ExtractZipFile.java
@@ -0,0 +1,67 @@
+package org.reallyenglish;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONException;
+
+public class ExtractZipFile extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+        if ("extract".equals(action)) {
+            extract(args, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void extract(CordovaArgs args, CallbackContext callbackContext) {
+        try {
+            String filename = args.getString(0);
+            String outputDirectory = args.getString(1);
+            outputDirectory += outputDirectory.endsWith(File.separator) ? "" : File.separator;
+
+            File file = new File(filename);
+            FileInputStream fis = new FileInputStream(file);
+            ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
+
+            ZipEntry entry;
+
+            byte[] buffer = new byte[1024];
+
+            while ((entry = zis.getNextEntry()) != null) {
+                String fileName = outputDirectory + entry.getName();
+                File outFile = new File(fileName);
+
+                if (entry.isDirectory()) {
+                    outFile.mkdirs();
+                } else {
+                    outFile.getParentFile().mkdirs();
+                    if(outFile.exists() || outFile.createNewFile()){
+                        FileOutputStream fout = new FileOutputStream(file);
+                        int count;
+                        while ((count = zis.read(buffer)) != -1) {
+                            fout.write(buffer, 0, count);
+                        }
+                        fout.close();
+                    }
+                }
+                zis.closeEntry();
+            }
+            zis.close();
+            callbackContext.success(filename);
+        } catch (Exception e) {
+          callbackContext.error("error occurred" + e.getMessage());
+        }
+    }
+}
